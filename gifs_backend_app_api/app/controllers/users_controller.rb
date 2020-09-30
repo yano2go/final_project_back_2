e4d75ae7796b17e7e1477c8f6ad62ff4a8324094
Alex Yanovich <alexyanovich@Alex.local>
2020-09-30 18:19:31 -0400

    got auth working

--- gifs_backend_app_api/app/controllers/users_controller.rb
+++ gifs_backend_app_api/app/controllers/users_controller.rb
@@ -1,51 +1,40 @@
+
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+before_action :authorized, only: [:auto_login]
 
-  # GET /users
-  def index
-    @users = User.all
-
-    render json: @users
+  # REGISTER
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
+    else
+      render json: {error: "Invalid username or password"}
+    end
   end
 
-  # GET /users/1
-  def show
+  # LOGGING IN
+  def login
+    @user = User.find_by(username: params[:username])
+    puts @user
+    if @user && @user.authenticate(params[:password])
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
+    else
+      render json: {error: "Invalid username or password"}
+      
+    end
+  end
+
+
+  def auto_login
     render json: @user
   end
 
-  # POST /users
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def user_params
-      params.require(:user).permit(:username, :password)
-    end
+  def user_params
+    params.permit(:username, :password, :age)
+  end
+
 end
