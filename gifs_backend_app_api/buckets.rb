31bb0d947490899e7b904cf3766f3cbe6da23907
Alex Yanovich <alexyanovich@Alex.local>
2020-10-01 13:12:09 -0400

    setting up active storage

--- gifs_backend_app_api/buckets.rb
+++ gifs_backend_app_api/buckets.rb
@@ -1,6 +1,6 @@
 require 'aws-sdk-s3'  # v2: require 'aws-sdk'
 
-s3 = Aws::S3::Resource.new(region: 'us-west-2')
+s3 = Aws::S3::Resource.new(region: 'us-east-2')
 
 s3.buckets.limit(50).each do |b|
   puts "#{b.name}"
